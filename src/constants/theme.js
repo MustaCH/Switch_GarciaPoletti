8265fde3c8c6aaf8288d5f84773961f9d29afef9
Musta <polettiignacio7@gmail.com>
2023-05-12 01:41:07 -0300

    modified theme.colors [primary/secondary]

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,7 +1,7 @@
 export const theme = {
   colors: {
-    primary: "#7851A9",
-    secundary: "#CB85D5",
+    primary: "#c73925",
+    secundary: "#611900",
     text: "#212121",
     black: "#000000",
     white: "#FFFFFF",
